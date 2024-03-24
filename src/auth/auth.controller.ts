9e8860f50acf756616a11bea6e43a7172b75583b
muhammadmuneebwaseem <muneebwaseem78@gmail.com>
2024-03-24 23:08:42 +0500

    authentication (login, register)

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  async signUp(
+    @Body()
+    signUpDto: SignUpDto,
+  ): Promise<{ token: string }> {
+    return this.authService.signUp(signUpDto);
+  }
+  @Get('/login')
+  async login(
+    @Body()
+    loginDto: LoginDto,
+  ): Promise<{ token: string }> {
+    return this.authService.login(loginDto);
+  }
+}
