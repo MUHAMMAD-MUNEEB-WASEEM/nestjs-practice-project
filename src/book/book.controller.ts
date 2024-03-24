43408f9834b047d873ae937c383b964b687e9046
muhammadmuneebwaseem <muneebwaseem78@gmail.com>
2024-03-24 18:26:31 +0500

    search and pagination

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -6,19 +6,23 @@ import {
   Param,
   Post,
   Put,
+  Query,
 } from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schema';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
-
+import { Query as ExpressQuery } from 'express-serve-static-core';
 @Controller('books')
 export class BookController {
   constructor(private bookService: BookService) {}
 
   @Get()
-  async getAllBooks(): Promise<Book[]> {
-    return this.bookService.findAll();
+  async getAllBooks(
+    @Query()
+    query: ExpressQuery,
+  ): Promise<Book[]> {
+    return this.bookService.findAll(query);
   }
 
   @Post()
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Book } from './schemas/book.schema';
 import * as mongoose from 'mongoose';
+import { Query } from 'express-serve-static-core';
 
 @Injectable()
 export class BookService {
@@ -10,8 +11,24 @@ export class BookService {
     private bookModel: mongoose.Model<Book>,
   ) {}
 
-  async findAll(): Promise<Book[]> {
-    const books = await this.bookModel.find();
+  async findAll(query: { keyword?: string; page?: string }): Promise<Book[]> {
+    const resPerPage = 2;
+    const currentPage = Number(query.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+    //search
+    const _keyword = query.keyword
+      ? {
+          title: {
+            $regex: query.keyword,
+            $options: 'i',
+          },
+        }
+      : {};
+
+    const books = await this.bookModel
+      .find(_keyword)
+      .limit(resPerPage)
+      .skip(skip);
     return books;
   }
 
